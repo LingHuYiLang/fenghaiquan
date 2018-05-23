9575f906bec7fbe38d2ee6bbd233f726e270f3d5
zhangsan <zhangsan@163.com>
2018-05-23 21:47:38 +0800

    zhangsan on master

--- test/login.py
+++ test/login.py
@@ -1,5 +1,9 @@
-<<<<<<< HEAD
 I am manager
-=======
 I am zhangsan
->>>>>>> 6fb89389961b7824a2dc3d182b21e8045fba0342
+
+i am zhangsan i is on master
+i am zhangsan i is on master
+i am zhangsan i is on master
+i am zhangsan i is on master
+i am zhangsan i is on master
+i am zhangsan i is on master
